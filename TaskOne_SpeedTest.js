ea011e6a9426e11bc1f500e23a15ed89b1a496c1
MonkAno <70213104+MonkAnoo@users.noreply.github.com>
2021-03-17 17:20:41 +0530

    Add files via upload

---
+++ TaskOne_SpeedTest.js
@@ -0,0 +1,17 @@
+function speedtest(speed) {
+
+    // const speedtest = 10;
+
+    if (speed <= 10) {
+        console.log("Slow");
+    } else if (speed > 10 && speed <= 40) {
+        console.log("Average");
+    } else if (speed > 40 && speed <= 100) {
+        console.log("Fast");
+    } else if (speed > 100 && speed <= 180) {
+        console.log("Very Fast");
+    } else {
+        console.log("Extermely Fast");
+    }
+}
+speedtest(181);
